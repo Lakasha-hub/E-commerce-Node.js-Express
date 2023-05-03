e8c933f78e10dd16f1bde5728c9e410ab24a04f2
Lakasha <lautaromoreira722@gmail.com>
2023-05-03 14:06:15 -0300

    5.0.1

--- src/public/js/realTimeProducts.js
+++ src/public/js/realTimeProducts.js
@@ -1,15 +1,6 @@
 import { createRowOfProducts } from "./functions.js";
 
 const socket = io();
-let products;
-
-fetch("/api/products/")
-  .then((res) => res.json())
-  .then((data) => {
-    products = data.products;
-    createRowOfProducts(products);
-  })
-  .catch((error) => console.log(error));
 
 socket.on("GetProductsUpdated", (productsUpdated) => {
   //Father Element
