4b545f1567d322f806c28e2a886502af466f7292
Lakasha <lautaromoreira722@gmail.com>
2023-07-03 16:19:40 -0300

    fix imports and add comments

--- src/middlewares/validateGetProductsParams.middleware.js
+++ src/middlewares/validateGetProductsParams.middleware.js
@@ -1,4 +1,4 @@
-import { productsService } from "../dao/mongo/manager/index.js";
+import { productService } from "../dao/mongo/manager/index.js";
 
 const validateGetQueryParams = async (req, res, next) => {
   const { limit = 10, page = 1, sort, query } = req.query;
