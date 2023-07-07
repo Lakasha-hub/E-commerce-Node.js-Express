3b34e33fe488146829a628dc3801d78b641314e4
Lakasha <lautaromoreira722@gmail.com>
2023-07-07 12:05:12 -0300

    add Cart in User Model

--- src/dao/mongo/models/cart.model.js
+++ src/dao/mongo/models/cart.model.js
@@ -17,12 +17,17 @@ const cartSchema = new mongoose.Schema(
         },
       },
     ],
+    default: [],
   },
   { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
 );
 
-cartSchema.pre("find", function(){ this.populate("products.product")})
-cartSchema.pre("findOne", function(){ this.populate("products.product")})
+cartSchema.pre("find", function () {
+  this.populate("products.product");
+});
+cartSchema.pre("findOne", function () {
+  this.populate("products.product");
+});
 
 const cartModel = mongoose.model(collection, cartSchema);
 
--- src/middlewares/attempsLimit.middleware.js
+++
@@ -1,4 +0,0 @@
-export const attempsLimitReached = async (req, res, next) => {
-    if (req.session.messages.length > 4) return res.status(400).json({ error: "Attempts limit reached" });
-    next()
-}
